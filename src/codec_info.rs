0f1f0c8293500e5f022cea1bddf9556f5ebc2db7
voluntas <nakai@shiguredo.jp>
2026-03-31 16:30:20 +0900

    ProbeContext の Drop を防御的に呼び出すようにする
    - Encoder / Decoder と同じ基準で Terminate / Release を if let Some で分岐する

--- src/codec_info.rs
+++ src/codec_info.rs
@@ -287,10 +287,16 @@ impl ProbeContext {
 
 impl Drop for ProbeContext {
     fn drop(&mut self) {
+        // Drop 内の panic は二重 panic で abort になるため、
+        // vtable の関数ポインタが欠けている場合は握りつぶす
         unsafe {
             let vtbl = &*(*self.context).pVtbl;
-            let _ = vtbl.Terminate.unwrap()(self.context);
-            vtbl.Release.unwrap()(self.context);
+            if let Some(terminate) = vtbl.Terminate {
+                let _ = terminate(self.context);
+            }
+            if let Some(release) = vtbl.Release {
+                release(self.context);
+            }
         }
     }
 }
